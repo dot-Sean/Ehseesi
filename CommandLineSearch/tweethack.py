fa511a3f172cdc10062ffaac016fcac388e202cd
Allen Lyons <allyons16@gmail.com>
2014-04-16 18:02:24 +0000

    changed tweethack.py to search for tweets specifically with links and orders based on followers

--- CommandLineSearch/tweethack.py
+++ CommandLineSearch/tweethack.py
@@ -1,4 +1,7 @@
 from twython import Twython
+import codecs
+from operator import itemgetter
+from collections import defaultdict
 from creds import *
 
 
@@ -11,14 +14,26 @@ while exit != True:
       if query.find("livestream") == -1 and query.find("live stream") == -1:
           query = query + " live stream"
       print query
-      result = twitter.search(q=query, result_type='recent')
+      result = twitter.search(q=query, result_type='recent', count=300)
       tweets = result['statuses']
+      ordered_results = {}
+      link_dict = {}
+      print result
+      count = 0
       for tweet in tweets:
-        parseThis = tweet['text']
-        linkIndex = parseThis.find("http://")
-        if linkIndex != (-1):
-         link = parseThis[linkIndex:]
-         print link[:link.find(' ')]
+        print tweet['user']['followers_count']
+        if tweet['user']['followers_count'] >= 10:
+          if len(tweet['entities']['urls'])>0:
+            parseThis = tweet['text']
+            ordered_results.setdefault(tweet['id'], tweet['user']['followers_count'])
+            link_dict.setdefault(tweet['id'],{}).setdefault(tweet['text'], tweet['entities']['urls'][0]['expanded_url'])
+      print link_dict
+      sorted_list = sorted(ordered_results.items(), key=itemgetter(1), reverse=True)
+      for key,value in sorted_list[:15]:
+        for k in link_dict[key]:
+          print k
+          print "***LINK*** :", link_dict[key][k]
+
       exit = raw_input("Type exit to go back or press enter to search for streams again.  ")
       if exit == "exit":
         exit = True
