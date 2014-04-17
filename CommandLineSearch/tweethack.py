d57ab62847c291831773e2680b306dbc98e46cf0
Allen Lyons <allyons16@gmail.com>
2014-04-17 09:39:32 -0400

    Changed tweethack.py to match my own copy
    This is terrible but there was a conflict with my pull request and this was easier and faster than figuring it out.

--- CommandLineSearch/tweethack.py
+++ CommandLineSearch/tweethack.py
@@ -1,4 +1,7 @@
 from twython import Twython
+import codecs
+from operator import itemgetter
+from collections import defaultdict
 from creds import *
 
 
@@ -11,14 +14,39 @@ while exit != True:
       if query.find("livestream") == -1 and query.find("live stream") == -1:
           query = query + " live stream"
       print query
-      result = twitter.search(q=query, result_type='recent')
+      result = twitter.search(q=query, result_type='recent', count=300)
       tweets = result['statuses']
+      # I created two dictionaries, one to sort tweets by how many followers the user hasand another to 
+      # match that tweet id to the text and the expanded urls
+      ordered_results = {}
+      link_dict = {}
+      
+      print result
+      # This count variable isn't used by anything but it was useful when testing to make sure the program
+      # was actually filtering out some tweets
+      count = 0
+      # For each tweet it checks the number of followers is greater than 10, and then checks that there is a 
+      # link in the tweet
+      # Then i stores information into the dictionaries and the continues to sort the tweets by follower and 
+      # finally it prints out the tweet text and the expanded url
       for tweet in tweets:
-        parseThis = tweet['text']
-        linkIndex = parseThis.find("http://")
-        if linkIndex != (-1):
-         link = parseThis[linkIndex:]
-         print link[:link.find(' ')]
+                        
+        if tweet['user']['followers_count'] >= 10:
+                          
+          if len(tweet['entities']['urls'])>0:
+            parseThis = tweet['text']
+            
+            ordered_results.setdefault(tweet['id'], tweet['user']['followers_count'])
+            link_dict.setdefault(tweet['id'],{}).setdefault(tweet['text'], tweet['entities']['urls'][0]['expanded_url'])
+            
+      sorted_list = sorted(ordered_results.items(), key=itemgetter(1), reverse=True)
+      for key,value in sorted_list[:15]:
+                        
+        for k in link_dict[key]:
+                          
+          print k
+          print "***LINK*** :", link_dict[key][k]
+
       exit = raw_input("Type exit to go back or press enter to search for streams again.  ")
       if exit == "exit":
         exit = True
