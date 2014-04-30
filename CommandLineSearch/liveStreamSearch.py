50d90515ef336bed7db67dc05a47b64d0f7ccc37
bdferr <ferrerih@live.unc.edu>
2014-04-30 00:51:38 +0000

    minor edits to search tool

--- CommandLineSearch/liveStreamSearch.py
+++ CommandLineSearch/liveStreamSearch.py
@@ -26,7 +26,7 @@ while exit != True:
       count = 0
       # For each tweet it checks the number of followers is at least 10, and then checks that there is a 
       # link in the tweet
-      # Then i stores information into the dictionaries and the continues to sort the tweets by follower and 
+      # Then it stores information into the dictionaries and then continues to sort the tweets by follower and 
       # finally it prints out the tweet text and the expanded URL
       
       file = open("results.txt", 'w')
@@ -42,19 +42,23 @@ while exit != True:
             link_dict.setdefault(tweet['id'],{}).setdefault(tweet['text'], tweet['entities']['urls'][0]['expanded_url'])
             
       sorted_list = sorted(ordered_results.items(), key=itemgetter(1), reverse=True)
+      print "These results are relevant to the query " + query + ":\n"
+      file.write ("These results are relevant to the query " + query + ":\n")
       for key,value in sorted_list[:15]:
                         
         for k in link_dict[key]:
                           
-          text = (k + " ***LINK*** :" + link_dict[key][k])
+          text = (k + " ***LINK*** : " + link_dict[key][k])
           text = text.encode("utf-8")
-          file.write("These results are relevant to the query", query, ":", "\n" text + "\n")
+          file_output = text + "\n"
+          print file_output
+          file.write(file_output)
           
           #print k
           #print "***LINK*** :", link_dict[key][k]
       file.close()
 
-      exit = raw_input("Type exit to go back or press enter to search for streams again.  ")
+      exit = raw_input("Type exit to go back or press enter to search for streams again. ")
       if exit == "exit":
         exit = True
         
